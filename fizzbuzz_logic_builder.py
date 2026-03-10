da71c7684e2e9d026de55903552ff22135303da1
Nisarga-V-17 <1mp22cg038@bgscet.ac.in>
2026-03-10 21:07:28 +0530

    Add files via upload

---
+++ fizzbuzz_logic_builder.py
@@ -0,0 +1,25 @@
+def fizzbuzz():
+    fizz_count = 0
+    buzz_count = 0
+    fizzbuzz_count = 0
+
+    for num in range(1, 51):
+        if num % 3 == 0 and num % 5 == 0:
+            print("FizzBuzz")
+            fizzbuzz_count += 1
+        elif num % 3 == 0:
+            print("Fizz")
+            fizz_count += 1
+        elif num % 5 == 0:
+            print("Buzz")
+            buzz_count += 1
+        else:
+            print(num)
+
+    print("\nCounts:")
+    print("Fizz:", fizz_count)
+    print("Buzz:", buzz_count)
+    print("FizzBuzz:", fizzbuzz_count)
+
+# function call
+fizzbuzz()
